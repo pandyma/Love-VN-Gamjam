9ba0e69f84e3a1ee8fb8f44f8e1de7e4400ad622
LanaStarsong <lanastarsong@gmail.com>
2021-02-27 18:31:31 -0700

    Revert "Revert "Revert "bug fixes"""
    This reverts commit e81ce93d3a850ceb6bb7cc295c6e9213ff5ad4d8.

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -1,20 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-
-
-public class GameScript : MonoBehaviour
+public class PauseScript : MonoBehaviour
 {
-
-    public UnityEvent methods;
-
-
-    public static bool isPaused = false;
-
-    public GameObject pauseMenu;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,35 +13,12 @@ public class GameScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    if (isPaused)
-        //    {
-        //        ResumeGame();
-        //    }
-        //    else
-        //    {
-        //        PauseGame();
-        //    }
-        //}
+        
     }
-    
+
+
     public void Quit()
     {
         Application.Quit();
-    }
-
-    public void ResumeGame()
-    {
-        Time.timeScale = 1;
-        isPaused = false;
-        pauseMenu.gameObject.SetActive(false);
-    }
-    
-    public void PauseGame()
-    {
-        Time.timeScale = 0;
-        isPaused = true;
-        pauseMenu.gameObject.SetActive(true);
     }
 }
